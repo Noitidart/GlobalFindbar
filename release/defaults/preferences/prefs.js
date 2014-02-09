90f006027d016e3da412d973a7b98b6ca03abbac
Noitidart <noitidart@gmail.com>
2014-02-09 14:05:36 -0800

    1.0
    download from amo the release version and put it in release folder, ill
    be working in this folder here

---
+++ release/defaults/preferences/prefs.js
@@ -0,0 +1,4 @@
+pref("extensions.jid0-GlobalFindBar@jetpack.restore_old_style", true);
+pref("extensions.jid0-GlobalFindBar@jetpack.tabScope", 0);
+pref("extensions.jid0-GlobalFindBar@jetpack.winScope", 0);
+pref("extensions.jid0-GlobalFindBar@jetpack.findbarFocus", 0);
